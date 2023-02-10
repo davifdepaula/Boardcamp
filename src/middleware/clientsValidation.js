4080f823e6954ca70cf45fc1f32c57fef132bcd9
davifdepaula <davi.ferreira100@gmail.com>
2023-02-10 20:44:28 -0300

    fix: return 409 when exist another client with same cpf

--- src/middleware/clientsValidation.js
+++ src/middleware/clientsValidation.js
@@ -18,7 +18,8 @@ const putClientsValidation = async(req, res, next) => {
   try{
     const validation = clientsSchema.validate(req.body)
     if(validation.error) return res.sendStatus(400)
-    const clients = await db.query(`SELECT * FROM customers where cpf = '${req.body.cpf} AND id <> ${req.params.id}'` )
+    const clients = await db.query(`SELECT * FROM customers WHERE cpf = '${req.body.cpf}' and id <> ${req.params.id}` )
+    console.log(clients.rows)
     if(clients.rows.length > 0) return res.sendStatus(409)
     next()
   }
