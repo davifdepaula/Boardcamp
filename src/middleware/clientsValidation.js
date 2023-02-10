7fa44be0918c085f9a1962eaa977c8c50acf4dc4
davifdepaula <davi.ferreira100@gmail.com>
2023-02-10 01:09:56 -0300

    fix: post/customers return 409 when cpf is already register

--- src/middleware/clientsValidation.js
+++ src/middleware/clientsValidation.js
@@ -5,7 +5,7 @@ const clientsValidation = async(req, res, next) => {
   try{
     const validation = clientsSchema.validate(req.body)
     if(validation.error) return res.sendStatus(400)
-    const clients = await db.query(`SELECT * FROM customers where name = '${req.body.name}'` )
+    const clients = await db.query(`SELECT * FROM customers where cpf = '${req.body.cpf}'` )
     if(clients.rows.length > 0) return res.sendStatus(409)
     next()
   }
